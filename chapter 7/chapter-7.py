a77088d386912e5239271324074736a596e6a7d1
WeijiaMa123 <31965430+WeijiaMa123@users.noreply.github.com>
2017-12-14 21:20:27 -0500

    hw123456

--- chapter 7/chapter-7.py
+++ chapter 7/chapter-7.py
@@ -1,44 +1,77 @@
-mylist=[1,2,4,5,6,7,9,32,54,-88,-7]
-mylist2=["frank","newbe","conming","xbnhk"]
-def count_odd(mylist):
+import sys
+
+def test(did_pass):
+    """  Print the result of a test.  """
+    linenum = sys._getframe(1).f_lineno   # Get the caller's line number.
+    if did_pass:
+        msg = "Test at line {0} ok.".format(linenum)
+    else:
+        msg = ("Test at line {0} FAILED.".format(linenum))
+    print(msg)
+
+mylist=[1,7,2,4,5,6,7,9,32,54,-88,-7]
+mylist2=["hello","babab","sam","frank","newbe","conming","xbnhk"]
+listnosam=["frank","wison","jaxsin","aronson"]
+def count_odd(lst):
     odd=0
-    for i in mylist:
+    for i in lst:
         if i%2 == 1:
             odd += 1
-    print(odd)
+    return odd
             
             
 
 
-def sum_even(mylist):
+def sum_even(llst):
     sumeven=0
-    for i in mylist:
+    for i in llst:
         if i%2==0:
             sumeven += i
-    print(sumeven)
+    return sumeven
             
     
-def sum_negative(mylist):
+def sum_negative(lllst):
     negasum=0
-    for i in mylist:
+    for i in lllst:
         if i < 0:
             negasum += i
-    print(negasum)
+    return negasum
     
-def countword5(mylist2):
+def countword5(list):
     numword5=0
-    for i in mylist2:
+    for i in list:
         if len(i)==5:
             numword5 += 1
-    print(numword5)
+    return numword5
 
+def sum_to_even(list):
+    mysum=0
+    for i in list:
+        if i%2 == 0:
+            break
+        mysum += i
+    return mysum
 
+def before_sam(lst):
+    count=0
+    for i in lst:
+        if i == "sam":
+            count += 1
+            break
+        count += 1
+    return count
             
- 
-    
 
-count_odd(mylist)
-sum_even(mylist)
-sum_negative(mylist)
-countword5(mylist2)
-sne(mylist)
+
+def test_suite():
+    test(count_odd(mylist)==6)
+    test(sum_even(mylist)==10)
+    test(sum_negative(mylist)==-95)
+    test(countword5(mylist2)==5)
+    test(sum_to_even(mylist)==8)
+    test(before_sam(mylist2)==3)
+    test(before_sam(listnosam)==4)
+
+test_suite()
+
+
