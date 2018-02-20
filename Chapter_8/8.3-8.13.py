e979d7f15b3310cfc2c85e02d86497de3f6869d5
WeijiaMa123 <31965430+WeijiaMa123@users.noreply.github.com>
2018-02-20 11:10:03 -0500

    pygame

--- Chapter_8/8.3-8.13.py
+++ Chapter_8/8.3-8.13.py
@@ -75,3 +75,5 @@ test_suite()
 print("i\ti*2\ti*3\ti*4\ti*5\ti*6\ti*7\ti*8\ti*9\ti*10\ti*11\ti*12")
 for i in range(1, 13):
     print(i, "\t", i*2, "\t", i*3, "\t", i*4, "\t", i*5, "\t", i*6, "\t", i*7, "\t", i*8, "\t", i*9, "\t", i*10, "\t", i*11, "\t", i*12 )
+
+
