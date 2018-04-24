87f756476e883d2169d3832160683fc25531a91b
WeijiaMa123 <31965430+WeijiaMa123@users.noreply.github.com>
2018-04-23 21:42:53 -0400

    module is hard

---
+++ chapter 12/12_1 calender.py
@@ -0,0 +1,20 @@
+import calendar
+#cal = calendar.TextCalendar()
+
+def firstday():
+    calendar.setfirstweekday(calendar.THURSDAY)
+    cal.pryear(2012)
+
+def birthday():
+    itermonthdates(2012, 10)
+
+
+
+
+#d = calendar.LocaleTextCalendar(6, "SPANISH")
+#d.pryear(2012)
+
+
+def leap():
+    calendar.isleap(2008)
+
